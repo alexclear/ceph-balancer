c9b154240d60c737862cb7669c45484fd35ce015
Alex Chistyakov <alexclear@gmail.com>
2025-01-12 00:55:19 +0300

    Set default target limits check

--- placementoptimizer.py
+++ placementoptimizer.py
@@ -5503,6 +5503,7 @@ def main():
                     balance_args.only_pool = None  # Set default pool filter
                     balance_args.ignore_ideal_pgcounts = "none"  # Set default pgcount ignore mode
                     balance_args.ignore_target_usage = False  # Set default target usage check
+                    balance_args.ensure_target_limits = False  # Set default target limits check
                     balance_output = ""
                     
                     def capture_output(output_str):
